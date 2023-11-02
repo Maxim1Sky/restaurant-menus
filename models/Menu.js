479f0e8daef70ba7b083803cc5a029692ac4abed
Maxim Toma <maxim.toma@sky.uk>
2023-11-02 13:37:10 +0000

    Created Menu table

--- models/Menu.js
+++ models/Menu.js
@@ -3,4 +3,14 @@ const { Sequelize, DataTypes, Model } = require("sequelize");
 
 // TODO - create a Menu model
 
+const Menu = db.define(
+  "Menu",
+  {
+    title: DataTypes.STRING,
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
 module.exports = { Menu };
