d4ba1c4b840828d8a3f172e2a96cbf309850b9c0
Maxim Toma <maxim.toma@sky.uk>
2023-11-02 14:28:51 +0000

    Added some tests, with the help of Zachary Whiteley, those test are 'interesting'

--- index.test.js
+++ index.test.js
@@ -10,13 +10,28 @@ describe("Restaurant and Menu Models", () => {
     // the 'sync' method will create tables based on the model class
     // by setting 'force:true' the tables are recreated each time the
     // test suite is run
-    await sequelize.sync({ force: true });
+    await db.sync({ force: true });
   });
 
   test("can create a Restaurant", async () => {
     // TODO - write test
-    //const newRest = await Restaurant.create({ name: "Indigo" });
-    expect("NO TEST").toEqual("EXPECTED DATA");
+
+    const newRest = await Restaurant.create({
+      name: "Indigo",
+      location: "London",
+      cuisine: "Thai",
+    });
+
+    let res = await Restaurant.findOne({ where: { name: "Indigo" } });
+    //res = JSON.stringify(res, null, 2);
+
+    const expectedCuisine = "Thai";
+    const expectedLocation = "London";
+    const expectedName = "Indigo";
+
+    expect(res.cuisine).toEqual(expectedCuisine);
+    expect(res.location).toEqual(expectedLocation);
+    expect(res.name).toEqual(expectedName);
   });
 
   test("can create a Menu", async () => {
